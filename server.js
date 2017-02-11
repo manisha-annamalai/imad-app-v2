30982c138139732e776d13d523a78890a0d43e4f
manisha-annamalai <manisha665@gmail.com>
2017-02-11 22:55:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,8 +79,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/myimg.jpg', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'myimg.jpg'));
+app.get('/ui/myimg.JPG', function (req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'myimg.JPG'));
 });
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
