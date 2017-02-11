4fcd730964ee19d36b605f2e948a9353e7228e4d
manisha-annamalai <manisha665@gmail.com>
2017-02-12 01:48:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,21 @@
-console.log('Loaded!');
-//change the text of the main content
-var element= document.getElementById('main-text');
-element.innerHTML='keep smiling';
-// move the image
-var img=document.getElementById('image');
-var marginLeft=0;
-function moveRight()
-{
-    marginLeft=marginLeft + 2;
-    img.style.marginLeft=marginLeft + 'px';
-}
-img.onclick = function()
-{ var interval = setInterval(moveRight, 20);
+//counter code
+var button = document.getElemnetById('counter');
+button.onclick = function(){
+    //create a req. object
+    var request = new XMLHttpRequest();
+    //capture the res and store it in a variable
+    request.onreadyStatechange= function (){
+        if(request.readystate === XMLHttpRequest.DONE){
+            //take some action
+            if(request.Status ====200){
+                var counter = request.responseText;
+                var span = document.getElemnetById('count');
+                span.innerHTML = counter.toString();
+            }
+    }
+    //not done yet
+};
+//make the request
+request.open('GET', 'http://manisha-annamalai.imad.hausura-app.io/counter', true);
+request.send(null);
 };
