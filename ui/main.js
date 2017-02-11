d6bbed8be5f6d3546b87df064f5e6f5518e7d87e
manisha-annamalai <manisha665@gmail.com>
2017-02-11 22:12:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,12 @@ var element= document.getElementById('main text');
 element.innerHTML='keep smiling';
 // move the image
 var img=document.getElementById('image');
+var marginleft=0;
+function moveright()
+{
+    marginleft=marginleft+10;
+    img.style.marginleft=marginleft+px;
+}
 img.onclick = function()
-{ img.style.marginLeft= '100px';
+{ var interval =setInterval(moveright, 100);
 };
