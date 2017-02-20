52d4426d9c54c2dfd25e61884e34715d7f98db6c
manisha-annamalai <manisha665@gmail.com>
2017-02-20 21:39:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,25 +26,26 @@ submit.onclick = function () {
     var request = new XMLHttpRequest();
     //capture the response and store it in a variable
     request.onreadystatechange = function (){
-    if(request.readyState=== XMLHttpRequest.DONE){
-        //take some action
-        if(request.status===200){
-            //capture a list of names and render it as a list
-            var name = request.responseText;
-            var names=JSON.parse(name);
-            var list= '';
-            for(var i=0; i<names.length; i++){
-                list += '<li>' + names[i] + '</li>';
+        if(request.readyState=== XMLHttpRequest.DONE){
+            //take some action
+            if(request.status===200){
+                //capture a list of names and render it as a list
+                var name = request.responseText;
+                var names=JSON.parse(name);
+                var list= '';
+                for(var i=0; i<names.length; i++){
+                    list += '<li>' + names[i] + '</li>';
+                }
+            var ul = document.getElementById('namelist');
+            ul.innerHTML= list;
             }
-        var ul = document.getElementById('namelist');
-        ul.innerHTML= list;
         }
-    }
+    };
     //not done yet
 
 //make the request
 var nameInput= document.getElementById('name');
 var name = nameInput.value;
-request.open('GET', 'http://manisha-annamalai.imad.hasura-app.io/submit-name?name=', name, true);
+request.open('GET', 'http://manisha-annamalai.imad.hasura-app.io/submit-name?name=' + name, true);
 request.send(null);
 };
